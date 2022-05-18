cebd600e85c05943840173c8607a979bbad109f8
Adel Ansari <adelansari@github.com>
2022-05-19 02:16:10 +0300

    a functioning bot with ping command only

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"discord-go-bot/bot"
+	"discord-go-bot/config"
+	"fmt"
+)
+
+func main() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
