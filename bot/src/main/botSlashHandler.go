58855283a99d48f4478843babfdb6cc35ff398e0
Adel Ansari <adelansari@github.com>
2022-05-24 20:14:26 +0300

    giveaway embed

--- bot/src/main/botSlashHandler.go
+++ bot/src/main/botSlashHandler.go
@@ -9,6 +9,22 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+type InteractionResponseData struct {
+	TTS             bool               `json:"tts"`
+	Content         string             `json:"content"`
+	Components      []MessageComponent `json:"components"`
+	Embeds          []*discordgo.MessageEmbed
+	AllowedMentions *MessageAllowedMentions `json:"allowed_mentions,omitempty"`
+	Flags           uint64                  `json:"flags,omitempty"`
+	Files           []*File                 `json:"-"`
+
+	// NOTE: autocomplete interaction only.
+	Choices []*ApplicationCommandOptionChoice `json:"choices,omitempty"`
+
+	CustomID string `json:"custom_id,omitempty"`
+	Title    string `json:"title,omitempty"`
+}
+
 func (scmSlash *SlashFeature) Ping(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	botLatency := s.HeartbeatLatency()
 	pongMessage := fmt.Sprintf("%s ", botLatency) + "pong!"
@@ -48,6 +64,22 @@ func (scmSlash *SlashFeature) Jokes(s *discordgo.Session, i *discordgo.Interacti
 		Data: &discordgo.InteractionResponseData{
 			TTS:     false,
 			Content: jokes[rand.Intn(len(jokes))],
+		},
+	})
+	if err != nil {
+		log.Fatal("could not fetch any jokes.", err)
+	}
+}
+
+func (scmSlash *SlashFeature) Giveaway(s *discordgo.Session, i *discordgo.InteractionCreate) {
+
+	giveawayEmbed := commands.Giveaway
+
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			TTS:    false,
+			Embeds: giveawayEmbed,
 		},
 	})
 	if err != nil {
