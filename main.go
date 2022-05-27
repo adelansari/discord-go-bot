7d3aba2c915de555a055a8a10b82205321fc8223
Adel Ansari <adelansari.a@gmail.com>
2022-05-27 00:26:59 +0000

    running server in replit

--- main.go
+++ main.go
@@ -2,18 +2,59 @@ package main
 
 import (
 	bot "discord-go-bot/bot/src/main"
+  "net/http"
+  "fmt"
+  // "log"
 )
 
 func main() {
-	// err := config.ReadConfig()
 
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
-
+  // Running bot
 	bot.Start()
+
+  //Create the default mux
+	mux := http.NewServeMux()
+
+	//Handling the /v1/teachers. The handler is a function here
+	mux.HandleFunc("/v1/teachers", teacherHandler)
+
+	//Handling the /v1/students. The handler is a type implementing the Handler interface here
+	sHandler := studentHandler{}
+	mux.Handle("/v1/students", sHandler)
+
+	//Create the server. 
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	s.ListenAndServe()
+
+  // // Go server
+  // http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+  // 	fmt.Fprintf(w, "Hello, world")
+  // })
+
+  // log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+
+  
 
 	<-make(chan struct{})
 	return
+}
+
+
+func teacherHandler(res http.ResponseWriter, req *http.Request) {
+  fmt.Fprintf(res, "Hello, world")
+	data := []byte("V1 of teacher's called")
+	res.WriteHeader(200)
+	res.Write(data)
+}
+
+type studentHandler struct{}
+
+func (h studentHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+  fmt.Fprintf(res, "Hello, world")
+	data := []byte("V1 of student's called")
+	res.WriteHeader(200)
+	res.Write(data)
 }
